498a7924ec23e1b0ce94323f45e6800cb2b4a09d
Richard Miller Wimmer <richardmmill@gmail.com>
2024-08-23 16:31:43 -0400

    System Math

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 // Console.WriteLine("Hello, World!");
 
 // Random dice = new Random();
-Random dice = new();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+// Random dice = new();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+using System.Security.Permissions;
+
+// Code Challenge
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = System.Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
