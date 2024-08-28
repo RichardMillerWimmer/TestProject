47cca46935da8dd51b8a10119896e5921b6a9294
Richard Miller Wimmer <richardmmill@gmail.com>
2024-08-28 12:29:18 -0400

    conventions and comments exercise

--- Program.cs
+++ Program.cs
@@ -135,11 +135,37 @@ using System.Linq.Expressions;
 // Console.WriteLine($"We have {sum} items in inventory.");
 
 //more array
-string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-foreach (string id in orderIDs)
-{
-    if (id.StartsWith("B")) 
-    {
-        Console.WriteLine(id);
+// string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+// foreach (string id in orderIDs)
+// {
+//     if (id.StartsWith("B")) 
+//     {
+//         Console.WriteLine(id);
+//     }
+// }
+
+// Conventions and Style
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+
+Array.Reverse(charMessage);
+int x = 0;
+
+foreach (char i in charMessage) 
+    { 
+        if (i == 'o')
+            {
+                x++;
+            }
     }
-}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310e1b7db64f13067901734432b69b3a68c89737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+041c5e5f1afda632b315773c623c1859b898df4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
